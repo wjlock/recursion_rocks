4b933d92f05dede09072b2ecca75d0a26d3fd7d9
Will <will.locklier@gmail.com>
2021-01-10 22:06:52 -0500

    Add max problem

--- find_max.py
+++ find_max.py
@@ -4,6 +4,15 @@
 # This function returns the largest number in a given array.
 
 def find_max(l):
+    if len(l) == 1:
+        return l[0]
+    else:
+        if len(l) > 1:
+            if l[0] > l[1]:
+                list.remove(l[1])
+            else:
+                list.remove(l[0])
+            find_max(l)
 
     # print(find_max([1, 4, 45, 6, -50, 10, 2]))
     # => 45
